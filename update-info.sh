998a6cb42b9e63fcbdcf0ca62631c71aabdf4f5d
Andris Reinman <andris@reinman.eu>
2021-12-28 16:47:13 +0200

    show code version in status

---
+++ update-info.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COMMIT_HASH=$(git rev-parse HEAD)
+TIMESTAMP=$(node -e 'console.log(Date.now())')
+cat >version-info.json <<EOL
+{
+    "commit": "${COMMIT_HASH}",
+    "time": "${TIMESTAMP}"
+}
+EOL
