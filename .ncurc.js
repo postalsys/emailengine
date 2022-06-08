1a2a5b4a2bb781fea5759a79f9240dd940521840
Andris Reinman <andris@reinman.eu>
2022-06-08 17:21:25 +0300

    v2.22.0

--- .ncurc.js
+++ .ncurc.js
@@ -1,7 +1,8 @@
 module.exports = {
     upgrade: true,
     reject: [
-        // v3 is ESM only. Node 18+ support native fetch so no reason to upgrade
-        'node-fetch'
+        // Block package upgrades that moved to ESM
+        'node-fetch',
+        'nanoid'
     ]
 };
