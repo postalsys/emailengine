3480d87fbc5288432837a0aab385fdd7b95e347f
Andris Reinman <andris@reinman.eu>
2022-10-17 10:49:12 +0300

    evaluate example code in a web worker process

---
+++ static/js/evaluation-worker.js
@@ -0,0 +1,14 @@
+/* global onmessage: true, postMessage: false */
+/* eslint no-unused-vars: 0 */
+'use strict';
+
+onmessage = e => {
+    console.log('Message received from main script', e.data);
+    try {
+        let fn = new Function('payload', e.data.code);
+        let result = fn(e.data.payload);
+        postMessage({ type: e.data.type, result });
+    } catch (err) {
+        postMessage({ type: e.data.type, error: err.message });
+    }
+};
