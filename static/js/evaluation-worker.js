7b9996dfaf7e0262a60aa4fecad8b7379b2aab23
Andris Reinman <andris@reinman.eu>
2023-06-28 14:59:07 +0300

    Added await and fetch support to filter/map function code editors

--- static/js/evaluation-worker.js
+++ static/js/evaluation-worker.js
@@ -4,9 +4,29 @@
 
 onmessage = e => {
     try {
-        let fn = new Function('payload', e.data.code);
-        let result = fn(e.data.payload);
-        postMessage({ type: e.data.type, result });
+        let source = `
+            return (async (payload) =>{
+                ${e.data.code}
+            })(payload);
+        `;
+
+        const logger = {};
+
+        for (let level of ['trace', 'debug', 'info', 'warn', 'error', 'fatal', 'silent']) {
+            logger[level] = msg => {
+                postMessage({ type: 'log', result: { level, msg: JSON.stringify(msg) } });
+            };
+        }
+
+        let fn = new Function('payload', 'logger', source);
+
+        fn(e.data.payload, logger)
+            .then(result => {
+                postMessage({ type: e.data.type, result });
+            })
+            .catch(err => {
+                postMessage({ type: e.data.type, error: err.message });
+            });
     } catch (err) {
         postMessage({ type: e.data.type, error: err.message });
     }
