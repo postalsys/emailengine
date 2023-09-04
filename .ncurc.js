39859fbe975f7b6bd04005ea164b4bed7018d881
Andris Reinman <andris@reinman.eu>
2023-09-04 08:44:18 +0300

    Started moving API routes to dedicated files - /v1/templates

--- .ncurc.js
+++ .ncurc.js
@@ -2,6 +2,9 @@ module.exports = {
     upgrade: true,
     reject: [
         // Block package upgrades that moved to ESM
-        'nanoid'
+        'nanoid',
+
+        // some kind of CVE in later versions. Only needed for license reference, so the actual version does not matter anyway
+        'startbootstrap-sb-admin-2'
     ]
 };
