cc7673764458b61995dca3850b40443edaeb1fb2
Andris Reinman <andris@reinman.eu>
2023-11-22 10:45:19 +0200

    chore: bumped dependencies

--- .ncurc.js
+++ .ncurc.js
@@ -4,6 +4,9 @@ module.exports = {
         // Block package upgrades that moved to ESM
         'nanoid',
 
+        // no support for Node 16
+        'marked',
+
         // some kind of CVE in later versions. Only needed for license reference, so the actual version does not matter anyway
         'startbootstrap-sb-admin-2'
     ]
