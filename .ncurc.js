df680e5c177b42c7b8175bf5dd831904be0d8140
Andris Reinman <andris@reinman.eu>
2023-12-07 11:19:35 +0200

    Bumped deps

--- .ncurc.js
+++ .ncurc.js
@@ -6,6 +6,7 @@ module.exports = {
 
         // no support for Node 16
         'marked',
+        'undici',
 
         // some kind of CVE in later versions. Only needed for license reference, so the actual version does not matter anyway
         'startbootstrap-sb-admin-2'
