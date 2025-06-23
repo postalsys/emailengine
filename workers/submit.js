531caadb00d8b7d8710668509845d9452c9c924d
Andris Reinman <andris@reinman.eu>
2025-06-23 18:45:27 +0300

    fix(submit): Added configuration ENV option EENGINE_SUBMIT_DELAY / --submitDelay=duration argument to allow rate limiting message sending (this is global, not account specific)

--- workers/submit.js
+++ workers/submit.js
@@ -60,6 +60,8 @@ const EENGINE_TIMEOUT = getDuration(readEnvValue('EENGINE_TIMEOUT') || config.se
 
 const SUBMIT_QC = (readEnvValue('EENGINE_SUBMIT_QC') && Number(readEnvValue('EENGINE_SUBMIT_QC'))) || config.queues.submit || 1;
 
+const SUBMIT_DELAY = getDuration(readEnvValue('EENGINE_SUBMIT_DELAY') || config.submitDelay) || null;
+
 let callQueue = new Map();
 let mids = 0;
 
@@ -325,7 +327,14 @@ const submitWorker = new Worker(
     },
     Object.assign(
         {
-            concurrency: SUBMIT_QC
+            concurrency: SUBMIT_QC,
+
+            limiter: SUBMIT_DELAY
+                ? {
+                      max: 1,
+                      duration: SUBMIT_DELAY
+                  }
+                : null
         },
         queueConf
     )
